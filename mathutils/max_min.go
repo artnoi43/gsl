8a3f4c97e45a1b4f7c13a9c85530901d009f5bb4
artworkk <prem@cleverse.com>
2022-08-28 02:21:53 +0700

    refactor(mathutils): Min and Max

--- mathutils/max_min.go
+++ mathutils/max_min.go
@@ -2,12 +2,8 @@ package mathutils
 
 import "golang.org/x/exp/constraints"
 
-type Number interface {
-	constraints.Integer | constraints.Float
-}
-
-func Max[N Number](slice []N) N {
-	var max N
+func Max[T constraints.Ordered](slice []T) T {
+	var max T
 	for _, num := range slice {
 		if num > max {
 			max = num
@@ -16,9 +12,10 @@ func Max[N Number](slice []N) N {
 	return max
 }
 
-func Min[N Number](slice []N) N {
+func Min[N constraints.Ordered](slice []N) N {
+	var zeroValue N
 	if len(slice) == 0 {
-		return 0
+		return zeroValue
 	}
 	min := slice[0]
 	for _, num := range slice {
