d1f9459dddb928e1feea6cdd88674cc848094796
artworkk <prem@cleverse.com>
2022-09-03 21:18:45 +0700

    feat(mglutils): move mathutils -> mglutils, and add more code

--- mglutils/array_test.go
+++ mglutils/array_test.go
@@ -1,12 +1,24 @@
 package mglutils
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestReverseSlice(t *testing.T) {
 	arr := []int{1, 2, 3, 4}
 	expecteds := []int{4, 3, 2, 1}
 
-	arr = Reverse(arr)
+	reversed := Reverse(arr)
+	for i, item := range reversed {
+		if item == arr[i] {
+			t.Fatal("not copied but in-place")
+		}
+		if expected := expecteds[i]; item != expected {
+			t.Fatalf("unexpected value, expecting %v, got %v", expected, item)
+		}
+	}
+
+	ReverseInPlace(arr)
 	for i, item := range arr {
 		if expected := expecteds[i]; item != expected {
 			t.Fatalf("unexpected value, expecting %v, got %v", expected, item)
--- mglutils/arrays.go
+++ mglutils/arrays.go
@@ -1,8 +1,15 @@
 package mglutils
 
-func Reverse[T any](arr []T) []T {
+func ReverseInPlace[T any](arr []T) {
 	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
 		arr[i], arr[j] = arr[j], arr[i]
 	}
-	return arr
+}
+
+func Reverse[T any](arr []T) []T {
+	reversed := make([]T, len(arr))
+	copy(reversed, arr)
+
+	ReverseInPlace(reversed)
+	return reversed
 }
