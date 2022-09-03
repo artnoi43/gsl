51a44c6e36400243cd23db7dc25884d251bf2f41
artnoi <gnu.pph@tutanota.com>
2022-09-04 02:39:52 +0700

    nothing(data): add comment for BFS funcs

--- data/graph/bfs.go
+++ data/graph/bfs.go
@@ -6,7 +6,8 @@ import (
 
 // BFSShortestPath calls BFSSearch and uses its output to call BFSShortestPathReconstruct.
 // It then returns the shortest path (slice of nodes), the number of hops it takes from `src` to `dst`,
-// and the boolean value whethere there's a path from `src` to `dst`
+// and a true boolean value if there's a path from `src` to `dst`.
+// Otherwise, a nil slice, -1, and false is returned if there's no such path.
 func BFSShortestPath(g Graph, src Node, dst Node) ([]Node, int, bool) {
 	rawPath, found := BFSSearch(g, src, dst)
 	if !found {
@@ -17,6 +18,7 @@ func BFSShortestPath(g Graph, src Node, dst Node) ([]Node, int, bool) {
 	return shortestPath, hops, found
 }
 
+// BFSSearch traverses the graph in BFS manner, and collecting VFS traversal information in a map `prev`. It returns the map, and a boolean value denoting if dst was reachable from src
 func BFSSearch(g Graph, src Node, dst Node) (map[Node]Node, bool) {
 	q := list.NewQueue[Node]()
 	q.Push(src)
@@ -49,6 +51,9 @@ func BFSSearch(g Graph, src Node, dst Node) (map[Node]Node, bool) {
 	return prev, found
 }
 
+// BFSShortestPathReconstruct reconstructs inclusive path from src to dst,
+// and returns the slice of reconstructed path. The path is backward, that is,
+// the first element is dst, and the last element is src.
 func BFSShortestPathReconstruct(backwardPath map[Node]Node, src, dst Node) ([]Node, int) {
 	current := dst
 	shortestPath := []Node{current}
