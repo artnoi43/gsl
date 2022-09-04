80e5e1018bdeb8ec1ef9973b4f2c811fe37128a9
artworkk <prem@cleverse.com>
2022-09-04 22:33:21 +0700

    refactor(data): add Directioner, NodeUtils, and EdgeUtils interface

--- data/graph/interface.go
+++ data/graph/interface.go
@@ -13,18 +13,37 @@ var ErrEdgeWeightNotNull = errors.New("found edge weight in unweighted graph")
 
 type nodeValue any
 
+type Directioner interface {
+	SetDirection(bool)
+	HasDirection() bool
+}
+
+type NodeUtils[N any] interface {
+	AddNode(node N)
+	GetNodes() []N
+}
+
+type EdgeUtils[N any, E any, W any] interface {
+	AddEdge(n1, n2 N, weight any) error
+	GetNodeEdges(node N) []E
+}
+
 // The Graph interface was modeled after wgraph.WeightedGraph, to try to make both interfaces
 // very similar, in case I'll be good at Go enough to compose a single interface.
 type Graph[T nodeValue] interface {
-	SetDirection(bool)
-	HasDirection() bool
-	AddNode(node Node[T])
+	Directioner
+	NodeUtils[Node[T]]
+	EdgeUtils[Node[T], Node[T], any]
+
+	// AddNode(node Node[T])
+	// GetNodes() []Node[T]
 	// AddEdge for unweighted graph DOES NOT do anything with weight when adding an edge.
 	// In the real implementation provided in this package, AddEdge returns an error if weight is not nil.
-	AddEdge(n1, n2 Node[T], weight any) error
-	GetNodes() []Node[T]
+	// AddEdge(n1, n2 Node[T], weight any) error
+	// GetNodeEdges(node Node[T]) []Node[T]
 	GetEdges() map[Node[T]][]Node[T]
-	GetNodeEdges(node Node[T]) []Node[T]
 }
 
-type Node[T nodeValue] data.Valuer[T]
+type Node[T nodeValue] interface {
+	data.Valuer[T]
+}
--- data/graph/wgraph/interface.go
+++ data/graph/wgraph/interface.go
@@ -3,6 +3,7 @@ package wgraph
 import (
 	"golang.org/x/exp/constraints"
 
+	"github.com/artnoi43/mgl/data/graph"
 	"github.com/artnoi43/mgl/data/list"
 )
 
@@ -11,15 +12,17 @@ type graphWeight interface {
 }
 
 type WeightedGraph[T graphWeight, S ~string] interface {
-	SetDirection(bool)
-	HasDirection() bool
-	AddNode(node WeightedNode[T, S])
-	AddEdge(n1, n2 WeightedNode[T, S], weight T) error
-	GetNodes() []WeightedNode[T, S]
+	graph.Directioner
+	graph.NodeUtils[WeightedNode[T, S]]
+	// graph.EdgeUtils[WeightedNode[T, S], WeightedEdge[T, S], T]
+
+	// AddNode(node WeightedNode[T, S])
+	// GetNodes() []WeightedNode[T, S]
 	// GetEdges gets all of the graph's edges.
-	GetEdges() map[WeightedNode[T, S]][]WeightedEdge[T, S]
 	// GetNodeEdges returns all the edge eminating from node
+	AddEdge(n1, n2 WeightedNode[T, S], weight T) error
 	GetNodeEdges(node WeightedNode[T, S]) []WeightedEdge[T, S]
+	GetEdges() map[WeightedNode[T, S]][]WeightedEdge[T, S]
 }
 
 type WeightedNode[T graphWeight, S ~string] interface {
