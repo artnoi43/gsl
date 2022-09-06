5a621a01e30ed4ade09b26bccc25a67df7446964
artworkk <prem@cleverse.com>
2022-09-06 23:31:40 +0700

    refactor(data): NodeWeighted[T, S] now embeds Node[T] instead of Valuer[T]

--- data/graph/wgraph/interface.go
+++ data/graph/wgraph/interface.go
@@ -3,7 +3,6 @@ package wgraph
 import (
 	"golang.org/x/exp/constraints"
 
-	"github.com/artnoi43/gsl/data"
 	"github.com/artnoi43/gsl/data/graph"
 )
 
@@ -16,11 +15,11 @@ type GraphWeighted[
 	T graphWeight,
 	S ~string,
 ] graph.GenericGraph[
-	// The graph's node type
+	// The weighted graph's node interface
 	NodeWeighted[T, S],
-	// The graph's edge type
+	// The weighted graph's edge interface
 	EdgeWeighted[T, S],
-	// The graph represents the edges as map of node to edges
+	// The weighted graph stores the edges as map of node to edges
 	map[NodeWeighted[T, S]][]EdgeWeighted[T, S],
 	// The edge weight can be of any types that implements graphWeight
 	T,
@@ -28,9 +27,9 @@ type GraphWeighted[
 
 // NodeWeighted should be able to put in a priority queue, just in case topological sort is needed.
 type NodeWeighted[T graphWeight, S ~string] interface {
-	data.Valuer[T] // For priority queue
-
-	// Other node methodds
+	// Inherit some from unweighted graphs
+	graph.Node[T]
+	// Other node with weighted edge methods
 	SetValueOrCost(value T)              // Save cost or value to the node
 	GetKey() S                           // Get the node's key, names, IDs
 	GetPrevious() NodeWeighted[T, S]     // When using with Dijkstra code, gets the previous (prior node) from in a Dijkstra walk.
