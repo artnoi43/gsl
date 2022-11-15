19d24ab88ac2b83ad3ff4e4ea8d20544eed106ac
artworkk <prem@cleverse.com>
2022-11-15 22:13:54 +0700

    feat(gslutils): add funcs for stringer

--- gslutils/upper_lower.go
+++ gslutils/upper_lower.go
@@ -2,10 +2,30 @@ package gslutils
 
 import "strings"
 
+type stringer interface {
+	String() string
+}
+
 func ToUpper[T ~string](s T) T {
 	return T(strings.ToUpper(string(s)))
 }
 
 func ToLower[T ~string](s T) T {
 	return T(strings.ToLower(string(s)))
+}
+
+func StringerToUpperString(s stringer) string {
+	return strings.ToUpper(s.String())
+}
+
+func StringerToLowerString(s stringer) string {
+	return strings.ToLower(s.String())
+}
+
+func StringerToUpper[T stringer](s T) T {
+	return T(StringerToUpper(s))
+}
+
+func StringerToLower[T stringer](s T) T {
+	return T(StringerToLower(s))
 }
