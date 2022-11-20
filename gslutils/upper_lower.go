263fceb8e9593604cd63d3593581e513246ff764
artworkk <prem@cleverse.com>
2022-11-20 23:04:09 +0700

    refactor(gslutils): remove useless functions

--- gslutils/upper_lower.go
+++ gslutils/upper_lower.go
@@ -14,18 +14,14 @@ func ToLower[T ~string](s T) T {
 	return T(strings.ToLower(string(s)))
 }
 
+// StringerToUpperString calls String() on s
+// and returns the result of calling `strings.ToUpper` on that string.
 func StringerToUpperString(s stringer) string {
 	return strings.ToUpper(s.String())
 }
 
+// StringerToLowerString calls String() on s
+// and returns the result of calling `strings.ToLower` on that string.
 func StringerToLowerString(s stringer) string {
 	return strings.ToLower(s.String())
-}
-
-func StringerToUpper[T stringer](s T) T {
-	return T(StringerToUpper(s))
-}
-
-func StringerToLower[T stringer](s T) T {
-	return T(StringerToLower(s))
 }
