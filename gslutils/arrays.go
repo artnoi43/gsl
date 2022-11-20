52d6700bdba7a3ae69b0586ad07eba8e1f8bd57c
artworkk <prem@cleverse.com>
2022-11-20 22:55:44 +0700

    feat(gslutils): add FilterSlice, DerefValues, DerefValuesIf, and refactor (BREAKING CHANGE) CollectPointers and CollectPointersIf to accept []T instead of *[]T

--- gslutils/arrays.go
+++ gslutils/arrays.go
@@ -14,6 +14,7 @@ func Reverse[T any](arr []T) []T {
 	return reversed
 }
 
+// Contains iterates over |arr| and returns true if an element in |arr| is equal to |item|.
 func Contains[T comparable](arr []T, item T) bool {
 	for _, elem := range arr {
 		if elem == item {
@@ -24,23 +25,96 @@ func Contains[T comparable](arr []T, item T) bool {
 	return false
 }
 
-func CollectPointers[T any](arr *[]T) []*T {
-	derefArr := *arr
-	out := make([]*T, len(derefArr))
+// FilterSlice iterates over |arr| and calls |filterFunc| on each of the element |elem|.
+// If filterFunc returns true, |elem| is added to the return slice.
+func FilterSlice[T any](arr []T, filterFunc func(elem T) bool) []T {
+	if arr == nil {
+		return nil
+	}
 
-	for i := range derefArr {
-		out[i] = &derefArr[i]
+	var filtered []T
+	for _, elem := range arr {
+		if filterFunc(elem) {
+			filtered = append(filtered, elem)
+		}
+	}
+
+	return filtered
+}
+
+// CollectPointers iterates over |arr| and returns a new slice containing all references
+// to the elements of |arr|.
+func CollectPointers[T any](arr []T) []*T {
+	if arr == nil {
+		return nil
+	}
+
+	out := make([]*T, len(arr))
+	for i := range arr {
+		out[i] = &arr[i]
 	}
 
 	return out
 }
 
-func CollectPointersIf[T any](arr *[]T, filterFunc func(T) bool) []*T {
-	derefArr := *arr
+// CollectPointersIf iterates over |arr| and calling filterFunc on the element |elem|.
+// If filterFunc returns true, |elem| is added to the return slice.
+func CollectPointersIf[T any](arr []T, filterFunc func(elem T) bool) []*T {
+	if arr == nil {
+		return nil
+	}
 	var filtered []*T
-	for i := range derefArr {
-		if filterFunc(derefArr[i]) {
-			filtered = append(filtered, &derefArr[i])
+	for i := range arr {
+		value := arr[i]
+		if filterFunc(value) {
+			filtered = append(filtered, &value)
+		}
+	}
+
+	return filtered
+}
+
+// DerefValues iterates over |arr| and return a new slice containing dereferenced values
+// of the elements in |arr|. If an element in |arr| is nil at index `i`, the value of the
+// return slice at index `i` is a default value for type T.
+func DerefValues[T any](arr []*T) []T {
+	if arr == nil {
+		return nil
+	}
+
+	var t T // Default value
+
+	values := make([]T, len(arr))
+	for i := range arr {
+		if arr[i] == nil {
+			values[i] = t
+			continue
+		}
+
+		values[i] = *arr[i]
+	}
+
+	return values
+}
+
+// DerefValuesIf iterates over |arr| and calls |filterFunc| on the element |elem|.
+// If filterFunc returns true, |elem|'s dereferenced value is added to the return slice.
+// Unlike DerefValues but similar to CollectPointersIf, if |elem| is nil,
+// its value will not be added to the return slice
+func DerefValuesIf[T any](arr []*T, filterFunc func(elem T) bool) []T {
+	if arr == nil {
+		return nil
+	}
+
+	var filtered []T
+	for _, elem := range arr {
+		if elem == nil {
+			continue
+		}
+
+		value := *elem
+		if filterFunc(value) {
+			filtered = append(filtered, value)
 		}
 	}
 
--- gslutils/arrays_test.go
+++ gslutils/arrays_test.go
@@ -14,14 +14,14 @@ func TestReverseSlice(t *testing.T) {
 			t.Fatal("not copied but in-place")
 		}
 		if expected := expecteds[i]; item != expected {
-			t.Fatalf("unexpected value, expecting %v, got %v", expected, item)
+			t.Fatalf("unexpected value -- expecting %v, got %v", expected, item)
 		}
 	}
 
 	ReverseInPlace(arr)
 	for i, item := range arr {
 		if expected := expecteds[i]; item != expected {
-			t.Fatalf("unexpected value, expecting %v, got %v", expected, item)
+			t.Fatalf("unexpected value -- expecting %v, got %v", expected, item)
 		}
 	}
 }
@@ -34,34 +34,80 @@ func TestContains(t *testing.T) {
 	if !Contains(arr0, i) {
 		t.Log("item", i)
 		t.Log("arr", arr0)
-		t.Fatalf("unexpected Contains result -- expected true")
+		t.Fatalf("unexpected Contains result -- expecting true")
 	}
 	if Contains(arr1, i) {
 		t.Log("item", i)
 		t.Log("arr", arr1)
-		t.Fatalf("unexpected Contains result -- expected false")
+		t.Fatalf("unexpected Contains result -- expecting false")
+	}
+}
+
+func TestFilterSlice(t *testing.T) {
+	arr := []int{-1, -2, 9, 69, 0}
+	actual := FilterSlice(arr, func(i int) bool {
+		return i <= 0
+	})
+	expecteds := []int{-1, -2, 0}
+
+	for i, elem := range actual {
+		if expected := expecteds[i]; elem != expected {
+			t.Fatalf("unexpected value -- expecting %d, got %d", expected, elem)
+		}
 	}
 }
 
 func TestCollectPointers(t *testing.T) {
 	arr := []int{1, 2, 6}
-	out := CollectPointers(&arr)
+	out := CollectPointers(arr)
 
 	for i := range arr {
 		if &arr[i] != out[i] {
-			t.Fatalf("wrong pointer collected - expected %p found %p\n", &arr[i], out[i])
+			t.Fatalf("wrong pointer collected -- expected %p found %p\n", &arr[i], out[i])
 		}
 	}
 
-	out = CollectPointersIf(&arr, func(i int) bool {
+	out = CollectPointersIf(arr, func(i int) bool {
 		return i > 5 // Only 6 should be filtered
 	})
 
 	if l := len(out); l != 1 {
-		t.Fatalf("wrong pointer filtered - expecting 1 result, got %d\n", l)
+		t.Fatalf("wrong pointer filtered -- expecting 1 result, got %d\n", l)
 	}
 
-	if &arr[len(arr)-1] != out[0] {
-		t.Fatalf("wrong pointer filtered - invalid pointer")
+	if arr[len(arr)-1] != *out[0] {
+		t.Fatalf("wrong pointer filtered -- invalid pointer")
+	}
+}
+
+func TestDerefValues(t *testing.T) {
+	type foo string
+
+	f1 := foo("henlo")
+	f2 := foo("lol")
+	f3 := foo("kuyhee")
+
+	arr := []*foo{&f1, &f2, nil, &f3}
+
+	// Test DerefValues
+	expecteds := []foo{f1, f2, foo(""), f3}
+	values := DerefValues(arr)
+	for i, actual := range values {
+		expected := expecteds[i]
+		if actual != expected {
+			t.Fatalf("unexpected value -- expecting %s, got %s", expected, actual)
+		}
+	}
+
+	// Test DerefValuesIf
+	expecteds = []foo{f1, f3}
+	values = DerefValuesIf(arr, func(elem foo) bool {
+		return len(elem) > 3
+	})
+	for i, actual := range values {
+		expected := expecteds[i]
+		if actual != expected {
+			t.Fatalf("unexpected value -- expecting %s, got %s", expected, actual)
+		}
 	}
 }
