a48c630f99df0fc13379e2c92a5c9e6043cb0ed2
artworkk <prem@cleverse.com>
2022-12-06 20:11:49 +0700

    fix(gslutils): fix Max/Min logic

--- gslutils/max_min.go
+++ gslutils/max_min.go
@@ -3,25 +3,33 @@ package gslutils
 import "golang.org/x/exp/constraints"
 
 func Max[T constraints.Ordered](items ...T) T {
-	var max T
+	if len(items) == 0 {
+		var zeroValue T
+		return zeroValue
+	}
+
+	max := items[0]
 	for _, num := range items {
 		if num > max {
 			max = num
 		}
 	}
+
 	return max
 }
 
-func Min[N constraints.Ordered](items ...N) N {
-	var zeroValue N
+func Min[T constraints.Ordered](items ...T) T {
 	if len(items) == 0 {
+		var zeroValue T
 		return zeroValue
 	}
+
 	min := items[0]
 	for _, num := range items {
 		if num < min {
 			min = num
 		}
 	}
+
 	return min
 }
