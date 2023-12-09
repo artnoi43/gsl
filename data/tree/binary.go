83f6eb523d63b9b3583dab744f1b44b638ad618e
artnoi43 <artdrawin@gmail.com>
2023-12-10 02:19:10 +0700

    refactor: binary tree

--- data/tree/binary.go
+++ data/tree/binary.go
@@ -1,51 +1,11 @@
 package tree
 
-type BinaryTree interface {
-	Parent(node int) int
-	LeftChild(node int) int
-	RightChild(node int) int
+type BinaryTree[POS any, NODE any] interface {
+	Parent(node POS) POS
+	LeftChild(node POS) POS
+	RightChild(node POS) POS
+	Node(pos POS) NODE
 
-	IsRoot(node int) bool
-	IsLeaf(node int) bool
-}
-
-type binaryTreeSlice[T any] struct {
-	backing []T
-}
-
-func (b *binaryTreeSlice[T]) Parent(node int) int {
-	return ParentIdx(node)
-}
-
-func (b *binaryTreeSlice[T]) LeftChild(node int) int {
-	return LeftChildIdx(node)
-}
-
-func (b *binaryTreeSlice[T]) RightChild(node int) int {
-	return RightChildIdx(node)
-}
-
-func (b *binaryTreeSlice[T]) IsRoot(node int) bool {
-	return node == 0
-}
-
-func (b *binaryTreeSlice[T]) IsLeaf(node int) bool {
-	return node >= len(b.backing)
-}
-
-func LeftChildIdx(parent int) int {
-	return (2 * parent) + 1
-}
-
-func RightChildIdx(parent int) int {
-	return LeftChildIdx(parent) + 1
-}
-
-func ParentIdx(child int) int {
-	rightChild := child%2 == 0
-	if rightChild {
-		return (child - 2) / 2
-	}
-
-	return (child - 1) / 2
+	IsRoot(node POS) bool
+	IsLeaf(node POS) bool
 }
---
+++ data/tree/binary_impl_slice.go
@@ -0,0 +1,51 @@
+package tree
+
+// Binary tree implementation with backing array
+type BinaryTreeSlice[T any] struct {
+	backing []T
+}
+
+func NewBinaryTreeSlice[T any]() BinaryTree[int, T] {
+	return &BinaryTreeSlice[T]{}
+}
+
+func (b *BinaryTreeSlice[T]) Parent(node int) int {
+	return ParentIdx(node)
+}
+
+func (b *BinaryTreeSlice[T]) LeftChild(node int) int {
+	return LeftChildIdx(node)
+}
+
+func (b *BinaryTreeSlice[T]) RightChild(node int) int {
+	return RightChildIdx(node)
+}
+
+func (b *BinaryTreeSlice[T]) Node(node int) T {
+	return b.backing[node]
+}
+
+func (b *BinaryTreeSlice[T]) IsRoot(node int) bool {
+	return node == 0
+}
+
+func (b *BinaryTreeSlice[T]) IsLeaf(node int) bool {
+	return node >= len(b.backing)
+}
+
+func LeftChildIdx(parent int) int {
+	return (2 * parent) + 1
+}
+
+func RightChildIdx(parent int) int {
+	return LeftChildIdx(parent) + 1
+}
+
+func ParentIdx(child int) int {
+	rightChild := child%2 == 0
+	if rightChild {
+		return (child - 2) / 2
+	}
+
+	return (child - 1) / 2
+}
