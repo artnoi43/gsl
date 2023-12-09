0e385a0e2c59f28f43ea50c8e492e47eab020825
artnoi43 <artdrawin@gmail.com>
2023-12-10 02:25:49 +0700

    feat: add inorder

--- data/tree/binary.go
+++ data/tree/binary.go
@@ -9,3 +9,19 @@ type BinaryTree[POS any, NODE any] interface {
 	IsRoot(node POS) bool
 	IsLeaf(node POS) bool
 }
+
+func InorderRecurse[POS any, NODE any](
+	tree BinaryTree[POS, NODE],
+	node POS,
+	f func(NODE) error,
+) error {
+	if err := InorderRecurse(tree, tree.LeftChild(node), f); err != nil {
+		return err
+	}
+
+	if err := f(tree.Node(node)); err != nil {
+		return err
+	}
+
+	return InorderRecurse(tree, tree.RightChild(node), f)
+}
