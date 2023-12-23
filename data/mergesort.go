1d4b1f079de8039cb3067d7d64ab330a1832d54f
prem <pphansuriyanon@gmail.com>
2023-12-24 05:42:03 +0700

    refactor: denest

--- data/mergesort.go
+++ data/mergesort.go
@@ -8,11 +8,11 @@ func MergeSort[T constraints.Ordered](arr []T, ordering SortOrder) []T {
 	length := len(arr)
 	lessFunc := LessFuncOrdered[T](ordering)
 
-	switch {
-	case length < 2:
+	if length < 2 {
 		return arr
+	}
 
-	case length == 2:
+	if length == 2 {
 		a, b := arr[0], arr[1]
 
 		if lessFunc(a, b) {
@@ -20,44 +20,43 @@ func MergeSort[T constraints.Ordered](arr []T, ordering SortOrder) []T {
 		}
 
 		return []T{b, a}
+	}
 
-	default:
-		mid := length / 2
+	mid := length / 2
 
-		left := MergeSort[T](arr[:mid], ordering)
-		right := MergeSort[T](arr[mid:], ordering)
+	left := MergeSort[T](arr[:mid], ordering)
+	right := MergeSort[T](arr[mid:], ordering)
 
-		sorted := make([]T, length)
-		var sp, lp, rp int
+	sorted := make([]T, length)
+	var sp, lp, rp int
 
-		for lp < len(left) && rp < len(right) {
-			l, r := left[lp], right[rp]
+	for lp < len(left) && rp < len(right) {
+		l, r := left[lp], right[rp]
 
-			if lessFunc(l, r) {
-				sorted[sp] = l
-				lp++
-				sp++
-
-				continue
-			}
-
-			sorted[sp] = r
-			rp++
-			sp++
-		}
-
-		for lp < len(left) {
-			sorted[sp] = left[lp]
+		if lessFunc(l, r) {
+			sorted[sp] = l
 			lp++
 			sp++
+
+			continue
 		}
 
-		for rp < len(right) {
-			sorted[sp] = right[rp]
-			rp++
-			sp++
-		}
-
-		return sorted
+		sorted[sp] = r
+		rp++
+		sp++
 	}
+
+	for lp < len(left) {
+		sorted[sp] = left[lp]
+		lp++
+		sp++
+	}
+
+	for rp < len(right) {
+		sorted[sp] = right[rp]
+		rp++
+		sp++
+	}
+
+	return sorted
 }
