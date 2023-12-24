c6ce547f96e636d1b37d48ae7d19070ff99f7a08
artnoi43 <artdrawin@gmail.com>
2023-12-25 02:52:45 +0700

    refactor: minimize call stack size

--- data/mergesort.go
+++ data/mergesort.go
@@ -6,7 +6,6 @@ import (
 
 func MergeSort[T constraints.Ordered](arr []T, ordering SortOrder) []T {
 	length := len(arr)
-	lessFunc := LessFuncOrdered[T](ordering)
 
 	if length < 2 {
 		return arr
@@ -14,6 +13,8 @@ func MergeSort[T constraints.Ordered](arr []T, ordering SortOrder) []T {
 
 	if length == 2 {
 		a, b := arr[0], arr[1]
+
+		lessFunc := LessFuncOrdered[T](ordering)
 
 		if lessFunc(a, b) {
 			return arr
@@ -30,6 +31,7 @@ func MergeSort[T constraints.Ordered](arr []T, ordering SortOrder) []T {
 	sorted := make([]T, length)
 	var sp, lp, rp int
 
+	lessFunc := LessFuncOrdered[T](ordering)
 	for lp < len(left) && rp < len(right) {
 		l, r := left[lp], right[rp]
 
