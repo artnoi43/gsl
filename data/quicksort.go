4929013a36e29560e8e531ad822acf642875a48c
artnoi43 <artdrawin@gmail.com>
2023-12-26 00:00:58 +0700

    refactor: quicksort tests

--- data/quicksort.go
+++ data/quicksort.go
@@ -39,44 +39,6 @@ func QuickSort[T constraints.Ordered](arr []T, ordering SortOrder) []T {
 	return sorted
 }
 
-func QuickSortGetter[T constraints.Ordered](
-	arr []Getter[T],
-	ordering SortOrder,
-) []Getter[T] {
-	l := len(arr)
-	// Base case
-	if l < 2 {
-		return arr
-	}
-
-	// Pivot here is going to be middle elem
-	mid := l / 2
-	pivot := arr[mid]
-
-	// pivoted is the list without the pivot element/member
-	pivoted := append(arr[:mid], arr[mid+1:]...) //nolint: gocritic
-	var left, right []Getter[T]                  //nolint:prealloc
-
-	{
-		// isLess should have lifetime of no more than the for-looop
-		// to help minimize stack size in huge recursive calls
-		isLess := LessFuncOrdered[T](ordering)
-		for _, elem := range pivoted {
-			if isLess(elem.GetValue(), pivot.GetValue()) {
-				left = append(left, elem)
-				continue
-			}
-
-			right = append(right, elem)
-		}
-	}
-
-	sorted := append(QuickSortGetter(left, ordering), pivot)
-	sorted = append(sorted, QuickSortGetter(right, ordering)...)
-
-	return sorted
-}
-
 func QuickSortCmp[T CmpOrdered[T]](arr []T, ordering SortOrder) []T {
 	l := len(arr)
 	// Base case
--- data/quicksort_test.go
+++ data/quicksort_test.go
@@ -5,55 +5,162 @@ import (
 	"testing"
 )
 
-func TestArraySortDirection(t *testing.T) {
-	if !Ascending.IsValid() {
-		t.Error("direction Ascending is invalid")
-	}
-	if !Descending.IsValid() {
-		t.Error("direction Descending is invalid")
-	}
+type testCaseQuickSort struct {
+	inputs   []int64
+	expected []int64
+	ordering SortOrder
+}
 
-	d := SortOrder(69)
-	if d.IsValid() {
-		t.Errorf("invalid direction %d is valid", d)
+func defaultTestCases() []testCaseQuickSort {
+	return []testCaseQuickSort{
+		{
+			inputs:   []int64{},
+			expected: []int64{},
+			ordering: Ascending,
+		},
+		{
+			inputs:   []int64{-1},
+			expected: []int64{-1},
+			ordering: Ascending,
+		},
+		{
+			inputs:   []int64{2, 2, 2},
+			expected: []int64{2, 2, 2},
+			ordering: Ascending,
+		},
+		{
+			inputs:   []int64{2, 3, 60, 1, 70, 234, -1},
+			expected: []int64{-1, 1, 2, 3, 60, 70, 234},
+			ordering: Ascending,
+		},
+		{
+			inputs:   []int64{2, 3, 60, 1, 70, 234, -1},
+			expected: []int64{234, 70, 60, 3, 2, 1, -1},
+			ordering: Descending,
+		},
 	}
 }
 
 func TestQuickSort(t *testing.T) {
-	arr := []int{2, 3, 60, 1, 70, 234, -1}
-	out := QuickSort(arr, Ascending)
-
-	if out[0] != -1 {
-		t.Fatal("unexpected result")
-	}
-	if out[len(out)-1] != 234 {
-		t.Fatal("unexpected result")
+	tests := defaultTestCases()
+	for i := range tests {
+		testCase := tests[i]
+		testQuickSort(t, testCase)
 	}
 
-	// See if it'll overflow
-	var s []int = make([]int, 1000000000)
-	for i := 0; i < 1000000000; i++ {
+	// See if it'll overflow on 10M ints
+	var s []int = make([]int, 10000000)
+	for i := 0; i < 10000000; i++ {
 		s[i] = i
 	}
 
-	QuickSort(arr, Descending)
+	QuickSort(s, Descending)
 }
 
-func TestQuickSortCmp(t *testing.T) {
-	ints := []int64{2, 3, 60, 1, 70, 234, -1}
-	arr := make([]*big.Int, len(ints))
+func testQuickSort(t *testing.T, testCase testCaseQuickSort) {
+	result := QuickSort(testCase.inputs, testCase.ordering)
 
-	for i := range ints {
-		arr[i] = big.NewInt(ints[i])
+	for i := range testCase.expected {
+		expected := testCase.expected[i]
+		actual := result[i]
+
+		if expected != actual {
+			t.Fatalf("got unexpected value at position %d: expecting %d, got %d", i, expected, actual)
+		}
+	}
+}
+
+type testCaseQuickSortCmp[T CmpOrdered[T]] struct {
+	inputs   []T
+	expected []T
+	ordering SortOrder
+}
+
+func TestQuickSortBigInts(t *testing.T) {
+	testCasesInt64 := defaultTestCases()
+
+	tests := make([]testCaseQuickSortCmp[*big.Int], len(testCasesInt64))
+	for i := range testCasesInt64 {
+		testCaseInt64 := &testCasesInt64[i]
+		inputs := make([]*big.Int, len(testCaseInt64.inputs))
+		expected := make([]*big.Int, len(testCaseInt64.expected))
+
+		for j := range inputs {
+			inputs[j] = big.NewInt(testCaseInt64.inputs[j])
+		}
+		for j := range expected {
+			expected[j] = big.NewInt(testCaseInt64.expected[j])
+		}
+
+		tests[i] = testCaseQuickSortCmp[*big.Int]{
+			inputs:   inputs,
+			expected: expected,
+			ordering: testCaseInt64.ordering,
+		}
 	}
 
-	out := QuickSortCmp(arr, Ascending)
+	for i := range tests {
+		testQuickSortCmp(t, tests[i])
+	}
+}
 
-	if out[0].Int64() != -1 {
-		t.Fatalf("unexpected result - expecting -1, got %d", out[0].Int64())
+type foo struct {
+	value int64
+}
+
+func (f *foo) Cmp(other *foo) int {
+	switch {
+	case f.value == other.value:
+		return 0
+	case f.value < other.value:
+		return 1
 	}
 
-	if out[len(out)-1].Int64() != 234 {
-		t.Fatalf("unexpected result - expecting 234, got %d", out[len(out)-1].Int64())
+	return -1
+}
+
+func TestQuickSortCustomCmp(t *testing.T) {
+	testCasesInt64 := defaultTestCases()
+	tests := make([]testCaseQuickSortCmp[*foo], len(testCasesInt64))
+
+	for i := range testCasesInt64 {
+		testCaseInt64 := testCasesInt64[i]
+		inputs := make([]*foo, len(testCaseInt64.inputs))
+		expected := make([]*foo, len(testCaseInt64.expected))
+
+		for j := range testCaseInt64.inputs {
+			inputs[j] = &foo{
+				value: testCaseInt64.inputs[j],
+			}
+		}
+		for j := range testCaseInt64.expected {
+			inputs[j] = &foo{
+				value: testCaseInt64.expected[j],
+			}
+		}
+
+		tests[i] = testCaseQuickSortCmp[*foo]{
+			inputs:   inputs,
+			expected: expected,
+			ordering: testCaseInt64.ordering,
+		}
+	}
+
+	for i := range tests {
+		testQuickSortCmp(t, tests[i])
+	}
+}
+
+func testQuickSortCmp[T CmpOrdered[T]](t *testing.T, testCase testCaseQuickSortCmp[T]) {
+	result := QuickSortCmp[T](testCase.inputs, testCase.ordering)
+
+	for i := range testCase.expected {
+		expected := testCase.expected[i]
+		actual := result[i]
+
+		if cmp := expected.Cmp(actual); cmp != 0 {
+			t.Logf("unexpected value at position %d - expecting %v, got %v", i, expected, actual)
+			t.Fatalf("unexpected Cmp result - expecting 0, got %d", cmp)
+		}
 	}
 }
